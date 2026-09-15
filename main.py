61e249e25a4242f6533f66a6d400db86757fb5ee
kokoapo <viniviscdiniz@gmail.com>
2026-09-15 19:50:42 -0300

    (feature) table info extraction

--- main.py
+++ main.py
@@ -10,9 +10,61 @@ def parse_html(url):
         return soup
     return None
 
+def extract_table_data(soup):
+    # Table Tag
+    table_tag = soup.find("table")
+    if not table_tag:
+        return None
+
+    # tr (Table Row) Tags
+    tr_table_tags = table_tag.find_all("tr")
+    tr_table_tags = tr_table_tags[1:]   # Remove Header Row
+    table_data = []
+    for tr_tag in tr_table_tags:
+        td_tags = tr_tag.find_all("td")
+        table_data.append(extract_table_row_data(td_tags))
+    return table_data
+
+def extract_table_row_data(td_tags):
+    name_tag = td_tags[1]
+    character_tag = td_tags[2]
+    release_tags = td_tags[3:5]
+    rarity_tag = td_tags[5]
+    stats_tags = td_tags[6:11]
+    tds_tags = td_tags[11:]
+
+    data = []
+
+    # Scrap Name
+    data.append(name_tag.b.a.text)
+
+    # Scrap Character
+    data.append(character_tag.text)
+
+    # Scrap Releases
+    for release_tag in release_tags:
+        data.append(release_tag.text if release_tag.text != "N/A" else None)
+
+    # Scrap Rarity
+    data.append(len(rarity_tag.text))   # Number of Stars
+
+    # Scrap Stats
+    for stats_tag in stats_tags:
+        data.append(int(stats_tag.text))
+
+    # Scrap Track / Distance / Style
+    for tds_tag in tds_tags:
+        data.append(tds_tag.span.a.attrs['href'][11])    # Find Image Ref, Get X from "/File:Rank_X.png"
+
+    return data
+
 if __name__ == "__main__":
     soup = parse_html(URL)
     if not soup:
         print("Failed to retrieve the webpage.")
         exit(1)
-    print(soup.prettify())
+
+    table_data = extract_table_data(soup)
+    if not table_data:
+        print("No table data found.")
+        exit(1)
